ddbd86a76807b7e213bfe7260b84ae17d1f995a0
sueshi-lab <phanpaporn.zcx@gmail.com>
2022-01-30 01:37:46 +0700

    Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      dropShadow: {
+        'b': '0 0 5px rgba(0, 0, 0, 0.15)',
+      }
+    },
+  },
+  plugins: [],
+}
