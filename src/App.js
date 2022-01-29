1a690319110d08be25d7e9eff40afb3d95588d15
sueshi-lab <phanpaporn.zcx@gmail.com>
2022-01-30 02:13:32 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,11 +6,6 @@ function App() {
     <div className='container-fluid bg-zinc-50 h-full'>
       <Navbar />
       <Landing />
-      {/* <div className="container-fluid">
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-      </div> */}
     </div>
   );
 }
