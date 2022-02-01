01c661b8be0a2610e69722d330ac74495eb65335
sueshi-lab <phanpaporn.zcx@gmail.com>
2022-02-02 02:12:17 +0700

    Update name in manifest.json

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -3,10 +3,10 @@ import ProfileImage from '../Asset/profile.jpg';
 export default function AboutMe() {
     return (
         <div id="aboutme" className="flex h-screen text-left">
-            <div className='m-auto text-lg px-8 max-w-4xl grid grid-flow-row-dense md:grid-cols-4'>
+            <div className='m-auto px-8 max-w-4xl grid grid-flow-row-dense md:grid-cols-4'>
                 <img className='max-w-sm w-full bg-cover object-cover md:h-full md:w-48 h-72 mb-6 rounded-lg' src={ProfileImage} />
                 <div className='m-auto md:col-span-3 md:p-7'>
-                    <p className='text-2xl font-medium uppercase'>Hi, everyone! 🙌</p>
+                    <p className='text-xl font-medium uppercase'>Hi, everyone! 🙌</p>
                     <p className='mt-4'>My name is Phanpaporn Zheng. I'm studying at KMUTT and expect to graduate in June 2022.
                         I'm seeking opportunities to learn and practice web development as a Developer.</p>
 
